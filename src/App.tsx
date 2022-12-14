3b9617d0c4ad8f8759a34894483bfb652280db47
MustafaMahdi <46289277+MustafaMahdi@users.noreply.github.com>
2022-12-14 15:43:59 +0200

    handle books endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import BookShelves from './pages/BookShelves';
 import Search from './pages/Search';
 import { Book as BookInterface } from "./models/book";
+import { ShelvesType } from './defines/defines';
 
 
 function App() {
@@ -17,26 +18,34 @@ function App() {
     )
   }, [])
 
-  const changeShelfHandler = (book: BookInterface, shelf: string) => {
-    BooksAPI.update(book, shelf);
+  const changeShelfHandler = async (updatedBook: BookInterface, shelf: string) => {
+    await BooksAPI.update(updatedBook, shelf);
+    checkIsBookExisted(updatedBook);
+    const updatedBooks = books.map(book => {
+      if (book.id === updatedBook.id) {
+        book.shelf = shelf as ShelvesType;
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  }
+  const checkIsBookExisted = (updatedBook: BookInterface) => {
+    const isBookExited = books.find(book => book.id === updatedBook.id);
+    if (!isBookExited) { books.push(updatedBook) };
 
-    BooksAPI.getAll().then((books) => {
-      setBooks(books)
-    }
-    )
   }
   return (
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/bookShelves" />
-        </Route>
-        <Route path="/bookShelves" exact>
-          <BookShelves books={books} changeShelfHandler={changeShelfHandler} />
-        </Route>
-        <Route path="/search" exact>
-          <Search books={books} changeShelfHandler={changeShelfHandler} />
-        </Route>
-      </Switch>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/bookShelves" />
+      </Route>
+      <Route path="/bookShelves" exact>
+        <BookShelves books={books} changeShelfHandler={changeShelfHandler} />
+      </Route>
+      <Route path="/search" exact>
+        <Search books={books} changeShelfHandler={changeShelfHandler} />
+      </Route>
+    </Switch>
   );
 }
 
